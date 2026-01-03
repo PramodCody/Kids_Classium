ac44c078e903cca84ade07fc77351903b7a8ff3d
PramodCody <pramodprajapatipk2@gmail.com>
2026-01-03 18:43:08 +0545

    Pop Game Prototype completed

--- Pop_Game/commons/bubble_pop.gd
+++ Pop_Game/commons/bubble_pop.gd
@@ -1,13 +1,21 @@
 extends Sprite2D
 
 signal poped
+signal automatic_poped
+
 var random_position = Vector2(randf_range(200, 2000), 1200)
 var random_number = randi_range(0, 3)
+var random_x_movement = randf_range(-1, 1)
 
 func MoveUp():
 	position.y -= 5
 	if position.y <= 0:
-		poped.emit()
+		automatic_poped.emit()
+		queue_free()
+	
+	position.x += random_x_movement
+	if position.x <= 0 or position.x >= 2400:
+		automatic_poped.emit()
 		queue_free()
 
 func _ready() -> void:
@@ -22,4 +30,3 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 		if $Label.text == "1":
 			poped.emit()
 			queue_free()
-		
--- Pop_Game/scripts/Poping_1/poping_1.gd
+++ Pop_Game/scripts/Poping_1/poping_1.gd
@@ -1,17 +1,34 @@
 extends Control
+@onready var progressbar = $ProgressBar
 
 var bubble_template = preload("res://Pop_Game/commons/bubble_pop.tscn")
+var isWin = false
 
 func _ready() -> void:
-	spawn_bubble()
-	spawn_bubble()
-	spawn_bubble()
+	var n = 6 #numbers of ball at once
+	for i in n:
+		spawn_bubble()
+		await get_tree().create_timer(1).timeout
+	
 
 func spawn_bubble():
-	var new_bubble = bubble_template.instantiate()
-	add_child(new_bubble)
-	new_bubble.poped.connect(on_bubble_poped)
+	if !isWin:
+		var new_bubble = bubble_template.instantiate()
+		add_child(new_bubble)
+		new_bubble.poped.connect(on_bubble_poped)
+		new_bubble.automatic_poped.connect(on_automatic_poped)
 
 
 func on_bubble_poped():
+	progressbar.value += 1
+	if progressbar.value >= 10:
+		$Retry_Button.show_with_fade()
+		$Next_Button.show_with_fade()
+		isWin = true
+		
+	await get_tree().create_timer(1).timeout
 	spawn_bubble() #whichever pops appears again
+
+func on_automatic_poped():
+	await get_tree().create_timer(1).timeout
+	spawn_bubble()
