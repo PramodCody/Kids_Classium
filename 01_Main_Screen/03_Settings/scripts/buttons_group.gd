93738b70a149e8ecf19b3b7f106b48bd2543f9fb
PramodCody <pramodprajapatipk2@gmail.com>
2026-01-15 11:41:31 +0545

    Settings UI Prototype Completed

--- 01_Main_Screen/03_Settings/scripts/buttons_group.gd
+++ 01_Main_Screen/03_Settings/scripts/buttons_group.gd
@@ -1,1 +1,11 @@
-extends Control
+extends VBoxContainer
+
+
+func _on_about_pressed() -> void:
+	$"../Action_Box/check".visible = false
+	%About_Dialog.visible = true
+
+
+func _on_sound_pressed() -> void:
+	%About_Dialog.visible = false
+	$"../Action_Box/check".visible = true
